5314d4fce735477c838bbebfc80bf82d1283aca9
Paulo Correia <paulo.correia@incognia.com>
2026-05-19 10:07:33 -0300

    fix: adds missing OPTIONS_REQUEST_TOKEN_MAX_LENGTH_KEY

--- android/src/main/java/com/incognia/reactnative/IncogniaModule.java
+++ android/src/main/java/com/incognia/reactnative/IncogniaModule.java
@@ -131,7 +131,7 @@ public class IncogniaModule extends ReactContextBaseJavaModule {
       boolean logEnabled = optionsParameters.hasKey(OPTIONS_LOG_ENABLED_KEY) ? optionsParameters.getBoolean(OPTIONS_LOG_ENABLED_KEY) : false;
       boolean locationEnabled = optionsParameters.hasKey(OPTIONS_LOCATION_ENABLED_KEY) ? optionsParameters.getBoolean(OPTIONS_LOCATION_ENABLED_KEY) : true;
       boolean installedAppsCollectionEnabled = optionsParameters.hasKey(OPTIONS_INSTALLED_APPS_COLLECTION_ENABLED_KEY) ? optionsParameters.getBoolean(OPTIONS_INSTALLED_APPS_COLLECTION_ENABLED_KEY) : false;
-      int requestTokenMaxLength = optionsParameters.hasKey(OPTIONS_REQUEST_TOKEN_MAX_LENGTH) ? optionsParameters.getInt(OPTIONS_REQUEST_TOKEN_MAX_LENGTH) : 8000; 
+      int requestTokenMaxLength = optionsParameters.hasKey(OPTIONS_REQUEST_TOKEN_MAX_LENGTH_KEY) ? optionsParameters.getInt(OPTIONS_REQUEST_TOKEN_MAX_LENGTH_KEY) : 8000; 
       
       IncogniaOptions options = new IncogniaOptions.Builder()
         .appId(appId)
